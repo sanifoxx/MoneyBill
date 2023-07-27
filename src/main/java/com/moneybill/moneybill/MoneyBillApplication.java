db5651c08576e597289f79950bbb25d78dfba0c3
EVMGit <macbook@air>
2023-07-27 23:00:22 +0500

    init commit

---
+++ src/main/java/com/moneybill/moneybill/MoneyBillApplication.java
@@ -0,0 +1,13 @@
+package com.moneybill.moneybill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyBillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyBillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moneybill/moneybill/MoneyBillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneybill.moneybill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyBillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
