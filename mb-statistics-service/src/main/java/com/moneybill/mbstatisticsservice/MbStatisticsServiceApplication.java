acf00e8364afe1cd6074506d3c65f630f1c421c3
EVMGit <macbook@air>
2023-08-05 22:07:45 +0500

    feat: Инициализирован сервис статистики

---
+++ mb-statistics-service/src/main/java/com/moneybill/mbstatisticsservice/MbStatisticsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.moneybill.mbstatisticsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbStatisticsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MbStatisticsServiceApplication.class, args);
+    }
+
+}
---
+++ mb-statistics-service/src/test/java/com/moneybill/mbstatisticsservice/MbStatisticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneybill.mbstatisticsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbStatisticsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
